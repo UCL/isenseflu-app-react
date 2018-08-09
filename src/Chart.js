9236537556d6424a1063e48edf00bdea2a168275
David Guzman <d.guzman@ucl.ac.uk>
2018-08-09 13:55:18 +0100

    Add area shade for confidence interval

--- src/Chart.js
+++ src/Chart.js
@@ -11,7 +11,23 @@ const data = {
 			label: "My First dataset",
       fill: false,
 			borderColor: "rgba(0, 123, 255, 1)",
+			backgroundColor: "rgba(63, 127, 191, 0.2)",
 			data: [1.65, 1.59, 1.80, 1.81, 1.56, 1.55, 2.40],
+			pointStyle: 'line'
+		},
+		{
+			label: "Upper confidence interval",
+			fill: false,
+			borderColor: "rgba(168, 198, 224, 1)",
+			data: [1.95, 1.99, 1.90, 1.91, 1.96, 1.85, 2.60],
+			pointStyle: 'line'
+		},
+		{
+			label: "Lower confidence interval",
+      fill: 1,
+			borderColor: "rgba(168, 198, 224, 1)",
+			backgroundColor: "rgba(63, 127, 191, 0.2)",
+			data: [0.65, 0.59, 0.80, 0.81, 1.06, 1.05, 1.40],
 			pointStyle: 'line'
 		}
 	]
