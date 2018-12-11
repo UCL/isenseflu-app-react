70a1ca50fd876334a5dd4e533383de1f6822ed31
David Guzman <d.guzman@ucl.ac.uk>
2018-12-11 13:12:13 +0000

    Update test for Averages test

--- src/Averages.test.js
+++ src/Averages.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import { createShallow } from '@material-ui/core/test-utils';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 
 import { Article } from './PublicTemplates';
 import AveragesComponent from './Averages';
@@ -10,4 +13,27 @@ it('renders About without crashing', () => {
   const modeldata = [];
   const wrapper = shallow(<AveragesComponent modeldata={modeldata} />);
   expect(wrapper.find(Article)).toHaveLength(1);
+});
+
+it('renders a table with the model name, region and average', () => {
+  const shallow = createShallow();
+  const modeldata = [
+    {
+      id: 1,
+      name: 'Model 1',
+      datapoints: [
+        { score_value: 0.5 },
+        { score_value: 1.0 },
+        { score_value: 1.5 }
+      ]
+    }
+  ];
+  const wrapper = shallow(<AveragesComponent modeldata={modeldata} />);
+  const row = wrapper.find(TableBody).find(TableRow);
+  expect(row).toHaveLength(1);
+  const cells = row.find(TableCell);
+  expect(cells).toHaveLength(3);
+  expect(cells.at(0).dive().childAt(0).text()).toEqual(modeldata[0].name);
+  expect(cells.at(1).dive().childAt(0).text()).toEqual('England');
+  expect(cells.at(2).dive().childAt(0).text()).toEqual('1');
 });
