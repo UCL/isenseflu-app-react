7c488a21b43503946504f1eda8222556ae059703
David Guzman <d.guzman@ucl.ac.uk>
2019-05-31 15:47:17 +0100

    Replace model_regions-n parameter for id in permalink. Closes #12

--- src/DataFiltering.test.js
+++ src/DataFiltering.test.js
@@ -43,6 +43,6 @@ it('generates permalink url', () => {
   const params = {...props, resolution, smoothing};
 
   const result = generatePermalinkUrl(params);
-  const expected = 'http://localhost/?start=2018-10-01&end=2018-11-01&resolution=day&smoothing=0&model_regions-0=1-e&model_regions-1=2-e';
+  const expected = 'http://localhost/?start=2018-10-01&end=2018-11-01&resolution=day&smoothing=0&id=1&id=2';
   expect(result).toEqual(expected);
 });
