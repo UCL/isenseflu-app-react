605592720e25cec558991c153b39d2d34f2f6a49
David Guzman <d.guzman@ucl.ac.uk>
2019-06-19 17:11:02 +0100

    Update comment docs in Url

--- src/Url.js
+++ src/Url.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 /**
  * Builds the URL used to fetch data for the Home component on componentDidMount(). Depending on
- * the value of the parameter 'source' it will map to the either /plink or /twlink paths
+ * the value of the parameter 'source' it will map to the either /plink or /twlink paths.
  * @param  {String} locationSearch String containing the value of location.search in the component
  * @return {String}                The URL to call the API
  */
@@ -23,6 +23,14 @@ export const homeFetchUrl = (locationSearch) => {
   return fetchUrl;
 };
 
+/**
+ * Builds the URL used to fetch the model scores for the Home component on the callback function
+ * handleChangeCallback() called by the modelcontrols prop in the Chart component.
+ * @param  {Number} id        The id of the model to retrieve the scores for
+ * @param  {String} startDate Start date of requested time period. In ISO format YYYY-MM-DD
+ * @param  {String} endDate   End date of requested time period. In ISO format YYYY-MM-DD
+ * @return {String}           The URL to call the API
+ */
 export const homeFetchScoresUrl = (id, startDate, endDate) => {
   let fetchUrl = process.env.REACT_APP_API_HOST;
   fetchUrl += fetchUrl.endsWith('/') ? '' : '/';
