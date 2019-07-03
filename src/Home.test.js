e79668e0a250b9a099800d8e0ac9e7c396ef9da5
David Guzman <d.guzman@ucl.ac.uk>
2019-07-03 19:34:04 +0100

    Clean test of handleChangeCallback from unused variables

--- src/Home.test.js
+++ src/Home.test.js
@@ -6,14 +6,11 @@ import HomeComponent from './Home';
 
 it('when event.target.checked is false the modelId is removed from activeModels', () => {
   const props = {
-    modeldata : [],
     location: { search: '' }
   }
-  const modelList = [ { id: 1, name: 'Model 1'}, { id: 2, name: 'Model 2' } ];
   const wrapper = shallow(<HomeComponent {...props}/>);
   const instance = wrapper.instance();
   instance.setState({
-    modelList: modelList,
     activeModels: [1, 2]
   });
   expect(wrapper.state('activeModels')).toEqual([1, 2]);
