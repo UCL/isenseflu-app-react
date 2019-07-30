dd8952eb81cb3f12dab13a5d921b6e3c75247f6b
David Guzman <d.guzman@ucl.ac.uk>
2019-07-30 14:07:18 +0100

    Lint ModelCheckBoxes and NavigationBar

--- src/ModelCheckboxes.js
+++ src/ModelCheckboxes.js
@@ -1,43 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
 
-export default class ModelCheckboxesComponent extends React.Component {
-
-	state = {
-		activeIds: []
-	}
+class ModelCheckboxesComponent extends React.Component {
+  state = {
+    activeIds: [],
+  }
 
   render() {
+    const { flagid, modellist, handleChangeCallback } = this.props;
 
-		const { flagid, modellist, handleChangeCallback } = this.props;
+    const { activeIds } = this.state;
 
-		const { activeIds } = this.state;
+    const models = modellist.map(model => (
+      <FormGroup key={model.id}>
+        <FormControlLabel
+          control={(
+            <Switch
+              value={String(model.id)}
+              checked={model.id === flagid || activeIds.includes(model.id)}
+              onChange={handleChangeCallback(model.id)}
+              color="primary"
+            />
+          )}
+          label={model.name}
+        />
+      </FormGroup>
+    ));
 
-    const models = modellist.map(model => {
-			return (
-				<FormGroup key={model.id}>
-					<FormControlLabel
-						control={
-							<Switch
-								value={String(model.id)}
-								checked={model.id === flagid || activeIds.includes(model.id)}
-								onChange={handleChangeCallback(model.id)}
-								color="primary"/>
-						}
-						label={model.name}
-					/>
-				</FormGroup>
-			);
-  	});
-
-		return (
-			<React.Fragment>
-				{models}
-			</React.Fragment>
-		);
-	}
-
+    return (
+      <React.Fragment>
+        {models}
+      </React.Fragment>
+    );
+  }
 }
+
+ModelCheckboxesComponent.propTypes = {
+  flagid: PropTypes.number.isRequired,
+  handleChangeCallback: PropTypes.func.isRequired,
+  modellist: PropTypes.object.isRequired,
+};
+
+export default ModelCheckboxesComponent;
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
@@ -6,10 +7,10 @@ import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
 import { withStyles } from '@material-ui/core/styles';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-import logo from './logo.svg'
+import logo from './logo.svg';
 
 const styles = {
   appBar: {
@@ -20,42 +21,38 @@ const styles = {
   },
   flexGrow: {
     flexGrow: 1,
-  }
+  },
 };
 
-const isActive = (pathname) => {
-  return (window.location.pathname === pathname) ? 'primary' : 'default';
+const isActive = pathname => (window.location.pathname === pathname ? 'primary' : 'default');
+
+const NavigationBar = (props) => {
+  const { classes } = props;
+
+  return (
+    <AppBar position="static" color="default" className={classes.appBar}>
+
+      <Toolbar>
+        <div className={classes.flexGrow}>
+          <img src={logo} className={classes.appLogo} alt="logo" />
+        </div>
+        <Button href="/" color={isActive('/')}>Home</Button>
+        <Button href="/about" color={isActive('/about')}>About</Button>
+        <Button href="/docs" color={isActive('/docs')}>Docs</Button>
+        <IconButton
+          className={classes.button}
+          aria-label="Twitter"
+          href="https://www.twitter.com/isenseflu"
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
-class NavigationBar extends React.Component {
-
-  render() {
-
-    const { classes } = this.props;
-
-    return (
-      <AppBar position="static" color="default" className={classes.appBar}>
-
-        <Toolbar>
-          <div className={classes.flexGrow}>
-            <img src={logo} className={classes.appLogo} alt="logo" />
-          </div>
-          <Button href="/" color={isActive('/')}>Home</Button>
-          <Button href="/about" color={isActive('/about')}>About</Button>
-          <Button href="/docs" color={isActive('/docs')}>Docs</Button>
-          <IconButton
-            className={classes.button}
-            aria-label="Twitter"
-            href="https://www.twitter.com/isenseflu"
-          >
-            <FontAwesomeIcon icon={faTwitter}/>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    );
-
-  }
-
+NavigationBar.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(NavigationBar);
