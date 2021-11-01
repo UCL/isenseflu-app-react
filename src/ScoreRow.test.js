56b1fde0bcd9d05220a06a53b21fd5b2568275db
David Guzman <d.guzman@ucl.ac.uk>
2021-11-01 15:02:06 +0000

    Migrate ScoreRow tests to testing library react

--- src/ScoreRow.test.js
+++ src/ScoreRow.test.js
@@ -1,19 +1,18 @@
 /* eslint react/jsx-props-no-spreading: "off" */
 import React from 'react';
 
-import { createMount } from '@material-ui/core/test-utils';
+import { render, screen } from '@testing-library/react';
 
 import ScoreRow from './ScoreRow';
 
-test('renders ScoreRowComponent wihout crashing', () => {
+test('renders ScoreRow wihout crashing', () => {
   expect.assertions(1);
   const props = {
     modelScores: [],
     scoreDate: '',
   };
-  const mount = createMount();
-  const wrapper = mount(<table><tbody><ScoreRow {...props} /></tbody></table>);
-  expect(wrapper.exists('ForwardRef(TableRow)')).toBe(true);
+  render(<table><tbody><ScoreRow {...props} /></tbody></table>);
+  expect(screen.getByRole('row')).toBeInTheDocument();
 });
 
 test('adds 2 columns of model scores; total of 3 columns', () => {
@@ -22,7 +21,6 @@ test('adds 2 columns of model scores; total of 3 columns', () => {
     modelScores: [{ model_id: 1, score_value: 1.0 }, { model_id: 2, score_value: 2.0 }],
     scoreDate: '2019-08-01',
   };
-  const mount = createMount();
-  const wrapper = mount(<table><tbody><ScoreRow {...props} /></tbody></table>);
-  expect(wrapper.find(ScoreRow).find('ForwardRef(TableCell)')).toHaveLength(3);
+  render(<table><tbody><ScoreRow {...props} /></tbody></table>);
+  expect(screen.getAllByRole('cell')).toHaveLength(3);
 });
